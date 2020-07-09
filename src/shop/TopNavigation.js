39c4c802ea7d91fdd82fa5e9359ec56674f16ac8
stefan22 <stefan.emailacct@gmail.com>
2020-07-09 17:56:12 +0100

    [nav] styles

--- src/shop/TopNavigation.js
+++ src/shop/TopNavigation.js
@@ -8,24 +8,27 @@ class TopNavigation extends Component {
 
 	render() {
 		return (
-      <>
-        <Link to={this.props.baseURL}
-          className="btn btn-secondary btn-block">All</Link>
+      <div className="site-wrap"  id="home-section">
+        <header className="site-navbar py-4 js-sticky-header site-navbar-target">
+          <div class="container">
+            <div class="row align-items-center">
 
-          { !!this.props.categories &&
-            this.props.categories.map(cat =>
+
+          <Link to={this.props.baseURL}
+            className="btn btn-secondary btn-block">All</Link>
+
+            { !!this.props.categories &&
+              this.props.categories.map(cat =>
               <Link key={cat}
                 to={`${this.props.baseURL}/${cat.toLowerCase()}`}
                 className="btn btn-secondary btn-block"
-              >
-                {cat}
-              </Link>
+              >{cat} </Link>
+            )}
 
-            )
-
-          }
-
-      </>
+              </div>
+            </div>
+         </header>
+      </div>
     )
  }
 }
